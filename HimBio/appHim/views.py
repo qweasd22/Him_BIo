70b92a51eca5b3c26494b9139da0b24ddf548486
qweasd22 <koltsov-main@mail.ru>
2024-10-15 15:39:34 +0300

    переделал нахуй все

--- HimBio/appHim/views.py
+++ HimBio/appHim/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.views import View
 
 def index(request):
     return render(request, 'index/index.html')
+
+def about(request):
+    return render(request, 'index/about.html')
+
+def faq(request):
+    return render(request, 'index/faq.html')
+
+def results(request):
+    return render(request, 'index/results.html')
+
+class LessonView(View):
+    def get(self, request):
+        context = {}
+        return render(request, 'lesson.html', context)
