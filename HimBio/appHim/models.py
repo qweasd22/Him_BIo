fa82fa17b97551d5be71d73c4a15d1733be6077f
qweasd22 <koltsov-main@mail.ru>
2024-10-15 21:36:56 +0300

    сделал хуйню, завтра доделать позионирование и функцию отправки мессенджа заявки на курсы

--- HimBio/appHim/models.py
+++ HimBio/appHim/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.user.username + " - " + self.date.strftime("%Y-%m-%d")
--- HimBio/appHim/views.py
+++ HimBio/appHim/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render 
 from django.views import View
+from .forms import RecordForm
+from django.shortcuts import redirect
 
 def index(request):
     return render(request, 'index/index.html')
@@ -17,3 +19,31 @@ class LessonView(View):
     def get(self, request):
         context = {}
         return render(request, 'lesson.html', context)
+    
+from .models import Record
+
+def home(request):
+    records = Record.objects.all()
+    return render(request, 'index.html', {'records': records})
+
+def record_create(request):
+    if request.method == 'POST':
+        form = RecordForm(request.POST)
+        if form.is_valid():
+            new_record = form.save(commit=False)
+            new_record.user = request.user
+            new_record.save()
+            return redirect('home')
+    else:
+        form = RecordForm()
+    return render(request, 'record_form.html', {'form': form})
+
+from django.core.mail import send_mail
+
+def send_notification(request):
+    subject = 'Новая запись пользователя'
+    message = 'Пользователь {} создал новую запись'.format(request.user.username)
+    recipient_list = ['koltsov-main@mail.ru']
+    send_mail(subject, message, 'noreply@example.com', recipient_list)
+
+    return redirect('home')
