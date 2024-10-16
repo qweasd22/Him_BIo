1c05572c0a3809c82729664482362530b2c793a9
qweasd22 <koltsov-main@mail.ru>
2024-10-16 18:32:43 +0300

    16.10

--- HimBio/appHim/models.py
+++ HimBio/appHim/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Record(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add=True)
-    message = models.TextField()
+class UserFormData(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.user.username + " - " + self.date.strftime("%Y-%m-%d")
+        return "name - " + self.name + ", email - " + self.email
